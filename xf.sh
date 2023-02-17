ab608699af67c0a9dc03d1df3b2ac619c7dd45cc
heiheisky <heiheisky@sina.com>
2023-02-17 14:35:44 +0800

    Update xf.sh
    update v2ray id

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "b387a58a-a1be-4874-ac26-787a15052c47",
                         "alterId": 0
                     }
                 ],
